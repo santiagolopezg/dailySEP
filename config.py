9bcc2455beb68bdd1a57e45c32e01f74a5c09855
Santiago LG <santiago.lopez.g@upch.pe>
2017-12-29 13:04:38 -0500

    updating twitbot

--- config.py
+++ config.py
@@ -18,14 +18,12 @@ dict_entry_url, dict_entry_date, dict_cp  = make_dicts()
 #start flag to schedule tweets; will be updated later on by schedule_tweet
 last_tweet_time = datetime.datetime.strptime("21/11/06 12:30", "%d/%m/%y %H:%M")
 last_update = datetime.datetime.now()
-p_counter = 0
 
 while True:
     try:
         message, dict_cp, last_tweet_time = schedule_tweet(last_tweet_time, dict_cp, dict_entry_url, last_update)
-        message = 'Entry # {0}: '.format(p_counter)+message
+        print message, len(message)
         api.update_status(message)
-        p_counter+=1
     except:
         print 'sleep for 6 hours...'
         time.sleep(21600)
--- dailysep.py
+++ dailysep.py
@@ -4,6 +4,9 @@
 import urllib2
 import random
 import datetime
+from HTMLParser import HTMLParser
+import re
+
 
 def html_to_string(url):
         
@@ -181,18 +184,195 @@ def update_dict(last_update, dict_entry_url):
         return dict_entry_url
 
 
+def get_entry_info(url, entry, l): # returns additional info to complete the 280 character limit (or less) 
+							# if it reaches a period after the limit. 
+		html_content = urllib2.urlopen(url).read()
+
+   		start_index = html_content.find('<p>')
+   		#print start_index
+
+   		end_index = html_content.find('</p>', start_index)
+   		#print end_index
+   		
+		#print url #smth like https://plato.stanford.edu/entries/antiochus-ascalon/
+   		
+   		new_html_content = html_content[start_index+3:end_index]
+   		
+   		#print len(new_html_content)
+   		
+   		#print new_html_content
+
+   		new_html_content = new_html_content.split('\n')
+   		
+   		text = ''
+   		for sentence in new_html_content:
+   			if len(sentence)>0:
+   				text+=' '+sentence
+   		text = text[1:]
+   		
+   		print text
+   		
+		#text = check_text(text)
+		text = ez_html(text)
+		
+		#print text
+
+		return text
+		
+
+
+def check_text(text):
+		new_text = ''
+		text = text.split(' ')
+		#print text
+		for word in text:
+			flag = 0
+		
+		## first check if word is only text without format. if it is, leave as is;
+			list_of_forbidden_items = ['&', '<', '>']
+			for letter in word:
+				if letter in list_of_forbidden_items:
+			
+					## is only text, no format:
+					flag = 0
+					break
+				else:
+					flag = 1
+			if flag == 1:
+				new_text += word+' '
+				
+			if flag == 0 : ## or else, remove formating and make plain text
+				#print 'go'
+				
+				if '&ldquo;' in word: #eg.: &ldquo;have
+					nword = word.split('&ldquo;')[1].split('&rdquo;')[0]
+								
+				if '<em>' in word: # eg.: <em>ceteris
+			
+					nword1 = word.split('<em>')[0]+word.split('<em>')[1]
+					if '</em>' in nword1:
+						nword = nword1.split('</em>')[0]
+					else:
+						nword = nword1
+					#print nword
+				
+				if '</em>' in word:
+					nword = word.split('</em>')[0]+word.split('</em>')[1]
+				
+				if 'href' in word:
+					nword = ''
+				
+				if '<sup>' in word or '</sup>' in word: # eg.: #<sup>[<a href="notes.html#1" name="note-1">1</a>]</sup>
+					nword = ''		
+						
+				if '&ndash;' in word: # eg.: 1075&ndash;1141)
+					nword = word.split('&ndash;')[0]+'-'+word.split('&ndash;')[1]
+					#print nword, 'dashed'
+				
+				if '&mdash;' in word: # Hume&mdash;had
+					nword = word.split('&mdash;')[0]+'-'+word.split('&mdash;')[1]
+
+				if '&lsquo;' in word:
+					nword = word.split('&lsquo;')[1]+"'"+word.split('&lsquo;')[1].split('&rsquo;')[0]
+				
+				if '&rsquo;' in word:
+					nword = word.split('&rsquo;')[0]+"'"+word.split('&rsquo;')[1]
+			
+				if 'oslash' in word:	#S&oslash;ren
+					nword = word.split('&oslash;')[0]+'o'+word.split('&oslash;')[1]			
+	
+				if 'eacute' in word: #Dieudonn&eacute
+					nword = word.split('&eacute;')[0]+'e'+word.split('&eacute;')[1]
+			
+				if '&#351;' in word: # Ku&#351;adas&#305;
+					nword = word.split('&#351;')[0]+'s'+word.split('&#351;')[1]
+				
+				if '&#257;' in word: # upam&#257;
+					nword = word.split('&#257;')[0]+'s'+word.split('&#257;')[1]
+				
+				if '&#305;' in word:	
+					nword = word.split('&#305;')[0]+'i'+word.split('&#305;')[1]
+				
+				if '(' in word and '(' not in nword:	
+					nword = '('+nword
+				
+				if ')' in word and ')' not in nword:
+					nword = nword+')'
+				
+				if ':' in word and ':' not in nword:
+					if 'here' in word or 'https' in word:
+						nword = word
+					else:
+						nword = nword+':'	
+			
+				try:
+					#print nword 
+					new_text += nword+' '
+				except:
+					print word
+				#print new_text
+			
+   		print new_text
+   		   		
+   		return new_text
+
+   		
+def ez_html(text):
+
+	parser = HTMLParser()
+	
+	ntext = parser.unescape(text)
+	
+	ntext = ntext.replace('<em>', '').replace('</em>', '').replace('<sup>', '').replace('</sup>', '').replace('C.E.', 'CE').replace('B.CE', 'BCE').replace('etc.', 'etc').replace('e.g.', 'eg').replace('J.J.', 'JJ').replace('G.W.F.', 'GWF')
+	
+	#remove hrefs, multiple spaces, etc.
+	ntext = re.sub("[[].*?[]]", "", ntext)
+	ntext = re.sub("<a.*?a>", "", ntext)
+	ntext = re.sub(' +',' ', ntext)
+	
+	return ntext
+
 def tweet(entry, url):
         #generates the tweet message. takes an entry name and corresponding url, and returns a string to be tweeted
         #would be cool to be able to add a few sentences copied from the web page.
 
-        msg = "{0}!\nLearn more about it here: {1}".format(entry, url)
-
-        #l = len(msg)
-
-        #add_info = get_entry_info(url, l)        
-
+        msg = "Learn more about {0} here: {1}".format(entry, url)
+         
+        #print 'foo'       
         
-        return msg
+        msg = ez_html(msg)
+        
+		#exit()
+        
+        #msg = check_text(msg)
+
+        #print msg, 'gooboo'
+				
+    	new_text = get_entry_info(url, entry, len(msg))
+    	
+    	print new_text
+    	
+        available_char = 280 - len(msg)
+
+        #print available_char
+   		
+        add_info = new_text[:available_char]
+
+		#print add_info
+
+		#keep only the complete sentences up to that point.
+  		
+        for letter in add_info[::-1]:
+        	if letter == '.': #and the next letter isn't a c:
+        		break
+        	else:
+        		add_info = add_info[:len(add_info)-1]
+				
+		#print add_info
+
+		tweet = add_info + ' ' + msg
+
+        return tweet
 
 
 def schedule_tweet(last_tweet_time, dict_cp, dict_entry_url, last_update):
@@ -207,7 +387,9 @@ def schedule_tweet(last_tweet_time, dict_cp, dict_entry_url, last_update):
         time_since_tweet = check_time - last_tweet_time
         time_since_tweet = time_since_tweet.seconds / 3600.0
         print str(time_since_tweet) + 'hours since last tweet'
-        if time_since_tweet >= 6.0:
+        #if time_since_tweet >= 6.0:
+        if time_since_tweet >= 0.0: ##debugging
+
                          
                 print 'time to post'
                 #this updates last_tweet_time
